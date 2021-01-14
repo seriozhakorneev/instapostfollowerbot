ad5edec036f68cdf4eb86203aaa1f3c4442244d1
Sergei <seriozhakorneev@gmail.com>
2021-01-14 15:24:01 +0300

    make it better

---
+++ bot.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+from classes import BotHandler, InstaPostParser, MongoHandler
+import threading
+from time import sleep
+
+help_message = 'Use the /add command to add the name of the Instagram user.\
+\nuse this format: <b>cristiano</b>, <b>davidbeckham</b>.\
+\nnot this: @cristiano or https://www.instagram.com/davidbeckham/\
+\n\nUse the /list command to see the names of the users you follow.\
+\n\nUse /delete to remove username.\
+\n\nmaximum users count: 10.'
+
+def replace_dots_in_insta_username(insta_username):
+	'''mongodb не принимает в key символы . $ '''
+	symbols = '🎱', '💰'
+	if any(symbol in insta_username for symbol in symbols):
+		with_dots = insta_username.replace('🎱', '.')
+		correct_username = with_dots.replace('💰', '$')
+	else:
+		without_dots = insta_username.replace('.', '🎱')
+		correct_username = without_dots.replace('$', '💰')
+
+	return correct_username
+
+def get_message_info(bot_obj, update):
+	'''собрать переменные от сообщения'''
+	update_id = update['update_id']
+	chat_id = bot_obj.get_chat_id(update)
+	message = bot_obj.get_message(update)
+	return update_id, chat_id, message
+
+def bot_message_update(bot_obj, offset_count, update_id):
+	'''получить новые сообщения боту'''
+	offset_count = update_id + 1
+	bot_obj.get_updates(offset_count)
+	last_update = bot_obj.get_last_update()
+	message = bot_obj.get_message(last_update)
+	return message
+
+def add_command(bot_obj, mongo_obj, chat_id, offset_count, update_id):
+	bot_obj.send_message(chat_id,
+		'Enter the Instagram username\n(format example: davidbeckham, lilpump)')
+	try:
+		new_page = bot_message_update(bot_obj, offset_count, update_id)
+		if new_page:
+			new_page = replace_dots_in_insta_username(new_page)
+			answer = mongo_obj.add_page(new_page)
+			return answer
+		else:
+			return 'Incorrect format'
+
+	except Exception:
+		return 'Timeout.Command was canceled'
+
+def list_command(mongo_obj):
+	user_data = mongo_obj.find_user_data()
+	if user_data:
+		if len(user_data) > 1:
+			list_str = ''
+			for i in user_data:
+				if i != '_id':
+					i = replace_dots_in_insta_username(i)
+					list_str +=f'<b>{i}</b>\n\n'
+			return list_str
+
+	return 'The list is empty'
+
+def delete_command(bot_obj, mongo_obj, chat_id, offset_count, update_id):
+	bot_obj.send_message(chat_id,'Enter the username to delete')
+	user_data = mongo_obj.find_user_data()
+	try:
+		del_element = bot_message_update(bot_obj, offset_count, update_id)
+		if del_element:
+			del_element = replace_dots_in_insta_username(del_element)
+			answer = mongo_obj.delete_page(del_element)
+			return answer
+		return 'Incorrect format'
+
+	except Exception:
+		return 'Timeout.Command was canceled'
+
+def generate_media_answer(media_links):
+	'''создать ссылки на медиа'''
+	media_str = ''
+	for link in media_links:
+		media_str += f'<a href="{link}"><b>link{media_links.index(link)+1}</b></a>  '
+	return media_str
+
+def generate_answer(*data):
+	'''проверки на закрытый акк
+	coздание ссылки'''
+	if data[3]:
+		media_str = generate_media_answer(data[3])
+
+	if data[0] and data[1]:
+		answer_str = f'<a href="{data[0]}"><b>{data[2]}</b></a>\n{data[1]}'
+		answer_str += '\n\n<b>media:</b>\n' + media_str
+	elif data[0]:
+		answer_str = f'<a href="{data[0]}"><b>post</b></a>'
+		answer_str += '\n\n<b>media:</b>\n' + media_str
+	else:
+		answer_str = None
+	return answer_str
+
+def send_insta_post_data(mongo_obj, username, user_data):
+	'''отправить данные с постов'''
+	check_for_new_posts = False
+	check_for_first_mailing = 0
+	inst_obj = InstaPostParser(username)
+	inst_obj.get_to_url(inst_obj.insta_id_url)
+	post_ids = inst_obj.get_first_7_post_ids()
+	username = replace_dots_in_insta_username(username)
+
+	if len(user_data[username]) == 0:
+		check_for_first_mailing = -1
+
+	if post_ids:
+		post_ids.reverse()
+		for post_id in post_ids[check_for_first_mailing:]:
+			if post_id not in user_data[username]:
+				check_for_new_posts = True
+				post_link = f'https://www.instagram.com/p/{post_id}/'
+				inst_obj.get_to_url(post_link)
+				name = inst_obj.get_name()
+				text = inst_obj.get_text()
+				media_links = inst_obj.get_media_links()
+				new_post = generate_answer(post_link, text, name, media_links)
+				bot_obj.send_message(chat_id, new_post)
+					
+		if check_for_new_posts:
+			mongo_obj.update_posts_list(username, post_ids)
+
+	inst_obj.close_browser()
+
+def send_insta_data(mongo_obj, bot_obj, chat_id):
+	'''достать данные с каждого	username'''
+	user_data = mongo_obj.find_user_data()
+	if user_data:
+		for username in user_data:
+			if username != '_id':
+				username = replace_dots_in_insta_username(username)
+				send_insta_post_data(mongo_obj, username, user_data)
+				
+def thread_mailing(bot_obj):
+	'''рассылка постов'''
+	def run():
+		while True:
+			if chat_id and mongo_obj:
+				send_insta_data(mongo_obj, bot_obj, chat_id)
+				sleep(1200)# 20минут
+
+	thread = threading.Thread(target=run)
+	thread.start()
+
+mongo_obj = None
+chat_id = None
+bot_obj = BotHandler()
+
+def main():
+	offset_count = None
+
+	while True:
+
+		bot_obj.get_updates(offset_count)
+		last_update = bot_obj.get_last_update()
+		# проверка на новые сообщения
+		if last_update:
+			global chat_id
+			update_id, chat_id, message = get_message_info(bot_obj, last_update)
+			# проверка сообщений на содержание только текста
+			if message:
+				global mongo_obj
+				mongo_obj = MongoHandler(chat_id)		
+				
+				if message == '/help':
+					bot_obj.send_message(chat_id, help_message)
+
+				elif message == '/add':
+					add_answer = add_command(bot_obj,mongo_obj,chat_id,
+										offset_count,update_id)
+					if not add_answer.startswith('Timeout'):
+						update_id += 1
+					bot_obj.send_message(chat_id, add_answer)
+					
+				elif message == '/list':
+					list_answer = list_command(mongo_obj)
+					bot_obj.send_message(chat_id, list_answer)
+
+				elif message == '/delete':
+					delete_answer = delete_command(bot_obj,mongo_obj,chat_id,
+											offset_count,update_id)
+					if not delete_answer.startswith('Timeout'):
+						update_id += 1
+					bot_obj.send_message(chat_id, delete_answer)
+
+			offset_count = update_id + 1
+	
+		sleep(5)
+
+if __name__ == '__main__':
+	try:
+		thread_mailing(bot_obj)
+		main()
+		
+	except KeyboardInterrupt:
+		exit()
---
+++ classes.py
@@ -0,0 +1,216 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from pymongo import MongoClient
+import requests
+import os
+
+# mongodb
+cluster = MongoClient("mongodb+srv://telegrambotmaker:tj1324Ip5Kmnh76@cluster0.lhzjo.mongodb.net/telegrambot?retryWrites=true&w=majority")
+db = cluster['instagrambot']
+channels_collection = db['instagram_page_ids']
+
+def cut_post_id_from_url(post_link):
+	'''достать post_id из ссылки'''
+	post_url_index = post_link.find('/p/')
+	post_id = ''
+	for letter in post_link[post_url_index+3:]:
+		if letter != '/':
+			post_id += letter
+	return post_id
+
+class MongoHandler:
+	
+	def __init__(self, chat_id):
+		self.channels_collection = channels_collection
+		# номер пользователя в телеграм чате с ботом
+		self.chat_id = chat_id
+
+	def find_user_data(self):
+		'''получить данные юзера по _id'''
+		return self.channels_collection.find_one({'_id': self.chat_id})
+
+	def add_page(self, page_id):
+		''' добавить page_id или создать юзера и его первый page_id'''
+		user_data = self.find_user_data()
+		if user_data:
+			if (len(user_data) - 1) < 10:
+				data_id = {'_id': self.chat_id}
+				data = {
+				'$set': {page_id : []}
+				}
+				self.channels_collection.update_one(data_id, data)
+				return 'Username added'
+			else:
+				return 'Limit exceeded (10)'
+		else:
+			data = {
+			'_id': self.chat_id, 
+			page_id: []
+			}
+			self.channels_collection.insert_one(data)
+			return 'Username added'
+
+	def update_posts_list(self, page_id, last_post):
+		'''добавить последний пост к page_id'''
+		data_id = {'_id': self.chat_id}
+		data = {'$set':{page_id : last_post}}
+		return self.channels_collection.update_one(data_id, data)
+
+	def delete_page(self, page_id):
+		'''удаляет 1 page_id из списка'''
+		user_data = self.find_user_data()
+		if user_data:
+			if page_id in user_data:
+				data_id = {'_id': self.chat_id}
+				data = {'$unset':{page_id : ''}}
+				self.channels_collection.update_one(data_id, data)
+				return 'Username deleted'
+			else:
+				return 'Incorrect data'
+		else:
+			return 'The list is empty'
+
+class InstaPostParser:
+
+	def __init__(self, insta_id):
+		self.op = webdriver.ChromeOptions()
+		self.op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+		self.op.add_argument("--headless")
+		self.op.add_argument("--no-sandbox")
+		self.op.add_argument("--disable-dev-sh-usage")
+		#self.PATH = '/home/sergei/code/test/instatest/app/chromedriver'
+		#self.driver = webdriver.Chrome(self.PATH)
+		self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=self.op)
+		self.insta_id = insta_id
+		self.insta_id_url = f'https://bibliogram.art/u/{self.insta_id}'
+
+	def get_to_url(self, link):
+		self.driver.get(link)
+
+	def get_first_7_post_ids(self):
+		'''вытаскиваем 7 последних post_id'''
+		post_ids = []
+		try:
+			get_post_div = WebDriverWait(self.driver, 10).until(
+				EC.presence_of_element_located((By.CSS_SELECTOR, ".timeline > section > div")))
+			for post_url in get_post_div.find_elements_by_tag_name('a')[:7]:
+				post_id = cut_post_id_from_url(post_url.get_attribute('href'))
+				post_ids.append(post_id)
+			return post_ids 
+		except:
+			return None
+
+	def get_media(self, media_type):
+		media_list = []
+		try:
+			get_media = WebDriverWait(self.driver, 10).until(
+				EC.presence_of_element_located((By.CSS_SELECTOR, '._97aPb.wKWK0')))
+			for element in get_media.find_elements_by_tag_name(media_type):
+				media_link = element.get_attribute('src')
+				media_list.append(media_link)
+		except:
+			return [None]
+
+		return media_list
+
+	def click_button(self):
+		'''переключить вложения поста'''
+		try:
+			button_next = WebDriverWait(self.driver, 10).until(
+				EC.element_to_be_clickable((By.CSS_SELECTOR, ".EcJQs > ._6CZji")))
+			button_next.click()
+
+			return button_next
+		except:
+			return None
+
+	def get_media_links(self):
+		media_links_list = []
+
+		button = self.click_button()
+		if button:
+
+			while button:
+				images = self.get_media('img')
+				videos = self.get_media('video')
+				media_links_list.extend(images)
+				media_links_list.extend(videos)
+				button = self.click_button()
+
+		else:
+			images = self.get_media('img')
+			videos = self.get_media('video')
+			media_links_list.extend(images)
+			media_links_list.extend(videos)
+
+		link_list = []
+		for link in media_links_list:
+			if (link not in link_list) and (link != None):
+				link_list.append(link)
+
+		return sorted(link_list, reverse=True)
+
+	def get_name(self):
+		try:
+			get_name = WebDriverWait(self.driver, 10).until(
+				EC.presence_of_element_located((By.CSS_SELECTOR, ".C4VMK > h2")))
+			return get_name.find_element_by_tag_name('a').text
+		except:
+			return None
+
+	def get_text(self):
+		try:
+			get_post_text = WebDriverWait(self.driver, 10).until(
+				EC.presence_of_element_located((By.CSS_SELECTOR, ".C4VMK > span")))
+			return get_post_text.text
+		except:
+			return None
+
+	def close_browser(self):
+		self.driver.quit()
+
+class BotHandler:
+
+	def __init__(self):
+		# telegram bot token
+		self.token = '1437349746:AAGzwB1DoMJzXA8c3vRi9Xmh2d6v6MO8x20'
+		# telegram bot api
+		self.telegram_api_url = f"https://api.telegram.org/bot{self.token}/"
+
+	def get_updates(self, offset=None, timeout=30):
+		'''получаем json с последними сообщениями по offset'''
+		method = 'getUpdates'
+		params = {'timeout': timeout, 'offset': offset}
+		response = requests.get(self.telegram_api_url + method, params)
+		result_json = response.json()['result']
+		return result_json
+
+	def get_last_update(self):
+		'''получаем последнее сообщение боту или None'''
+		get_result = self.get_updates()
+		
+		if len(get_result) > 0:
+			last_update = get_result[-1]
+		else:
+			return None
+		return last_update
+
+	def get_chat_id(self, last_update):
+		'''получить chat_id последнего сообщение'''
+		chat_id = last_update['message']['chat']['id']
+		return chat_id
+
+	def get_message(self, last_update):
+		'''получить текст последнего сообщение'''
+		message = last_update.get('message').get('text')
+		return message
+
+	def send_message(self, chat_id, message):
+		'''отправить сообщение'''
+		params = {'chat_id': chat_id, 'text': message, 'parse_mode': 'HTML'}
+		method = 'sendMessage'
+		response = requests.post(self.telegram_api_url + method, params)
+		return response
